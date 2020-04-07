b188db8b7276ef22c9c34b23522a4403bdc1a0af
Scar26 <mmatty26@gmail.com>
2020-04-07 12:47:26 +0530

    add pkcs7 unpad
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- Set2/Challenge9.py
+++ Set2/Challenge9.py
@@ -2,5 +2,11 @@ def pkcs7(s, l):
     byte = l-len(s)
     return s + bytes([byte])*byte
 
+def pkcs7_unpad(s):
+    byte = s[-1]
+    if s[-byte:] == bytes([byte])*byte:
+        s = s[:-byte]
+    return s
+
 if __name__ == '__main__':
     print(pkcs7(b"YELLOW SUBMARINE", 20))
